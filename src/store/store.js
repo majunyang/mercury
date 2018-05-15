ca2f04f4ff3f81576e420a44a5ab4b80fc279ed2
zyndev <zyndev@gmail.com>
2018-05-15 16:53:19 +0800

    init

---
+++ src/store/store.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import cookie from '../assets/util/cookie'
+
+Vue.use(Vuex)
+
+const userInfo = {
+  name: cookie.getCookie('name') || '',
+  token: cookie.getCookie('token') || ''
+}
+
+const store = new Vuex.Store({
+  state: {
+    login: true,
+    isLoading: false,
+    userInfo: userInfo
+  },
+  mutations: {
+    setUserInfo (state) {
+      state.userInfo = {
+        name: cookie.getCookie('name'),
+        token: cookie.getCookie('token'),
+      }
+    }
+  }
+})
+
+export default store
